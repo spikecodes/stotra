8d56a7c1502944e541ab6091945f173e8177b1d7
spikecodes <19519553+spikecodes@users.noreply.github.com>
2023-08-12 16:45:21 -0700

    Create Buy/Sell UI Panel

--- app/src/pages/StockView.tsx
+++ app/src/pages/StockView.tsx
@@ -1,6 +1,29 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Stat, StatArrow, Heading, Spacer } from "@chakra-ui/react";
+import {
+	Text,
+	Tabs,
+	TabList,
+	TabPanels,
+	Tab,
+	TabPanel,
+	Stat,
+	StatArrow,
+	Heading,
+	Spacer,
+	Flex,
+	Box,
+	Button,
+	HStack,
+	Stack,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper,
+	Divider,
+	Center,
+} from "@chakra-ui/react";
 import axios from "axios";
 import StockChart from "../components/StockChart";
 
@@ -11,6 +34,8 @@ const formatter = new Intl.NumberFormat("en-US", {
 
 function StockView() {
 	const { ticker } = useParams();
+
+	const [shares, setShares] = useState(0);
 
 	const [stock, setStock] = useReducer(
 		(state: any, newState: any) => ({ ...state, ...newState }),
@@ -32,24 +57,89 @@ function StockView() {
 	return (
 		<>
 			{stock.price > 0 && (
-				<>
-					<Stat>
-						<Heading size="md" fontWeight="md">
-							{stock.longName}
-						</Heading>
-						<Heading size="xl">{formatter.format(stock.price)}</Heading>
-						<Heading size="md">
-							<StatArrow
-								type={stock.changePercent > 0 ? "increase" : "decrease"}
-							/>
-							{stock.changePercent.toFixed(2)}%
-						</Heading>
-					</Stat>
+				<Flex direction={{ base: "column-reverse", md: "row" }} gap={5}>
+					<Box flex="0.75">
+						<Stat>
+							<Heading size="md" fontWeight="md">
+								{stock.longName}
+							</Heading>
+							<Heading size="xl">{formatter.format(stock.price)}</Heading>
+							<Heading size="md">
+								<StatArrow
+									type={stock.changePercent > 0 ? "increase" : "decrease"}
+								/>
+								{stock.changePercent.toFixed(2)}%
+							</Heading>
+						</Stat>
 
-					<Spacer height={5} />
+						<Spacer height={5} />
 
-					<StockChart ticker={ticker as string} />
-				</>
+						<StockChart ticker={ticker as string} />
+					</Box>
+					<Box flex="0.25" borderWidth="1px" borderRadius="md" p={5}>
+						<Tabs>
+							<TabList>
+								<Tab>Buy {ticker}</Tab>
+								<Tab>Sell {ticker}</Tab>
+							</TabList>
+
+							<Stack p="5">
+								<HStack>
+									<Text>Shares</Text>
+									<Spacer />
+									<NumberInput
+										defaultValue={1}
+										min={1}
+										width="20"
+										onChange={(e) => setShares(parseInt(e))}
+									>
+										<NumberInputField />
+										<NumberInputStepper>
+											<NumberIncrementStepper />
+											<NumberDecrementStepper />
+										</NumberInputStepper>
+									</NumberInput>
+								</HStack>
+								<HStack>
+									<Text>Current Price</Text>
+									<Spacer />
+									<Text>{formatter.format(stock.price)}</Text>
+								</HStack>
+								<Spacer />
+								<Divider />
+								<Spacer />
+								<HStack fontWeight="bold">
+									<Text>Total</Text>
+									<Spacer />
+									<Text>{formatter.format(stock.price * shares)}</Text>
+								</HStack>
+							</Stack>
+
+							<TabPanels>
+								<TabPanel>
+									<Button colorScheme="teal" size="lg" width="100%">
+										Buy
+									</Button>
+									<Center mt={3}>
+										<Text fontWeight="bold" fontSize="sm">
+											$6,000.00 Buying Power Available
+										</Text>
+									</Center>
+								</TabPanel>
+								<TabPanel>
+									<Button colorScheme="teal" size="lg" width="100%">
+										Sell
+									</Button>
+									<Center mt={3}>
+										<Text fontWeight="bold" fontSize="sm">
+											14 Shares Available
+										</Text>
+									</Center>
+								</TabPanel>
+							</TabPanels>
+						</Tabs>
+					</Box>
+				</Flex>
 			)}
 		</>
 	);
