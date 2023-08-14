0104836900477dae1e74c0ee884e55b7e6f65a1c
spikecodes <19519553+spikecodes@users.noreply.github.com>
2023-08-13 20:18:51 -0700

    Turn TransactionPane into a component and add button spinner

---
+++ app/src/components/TransactionPane.tsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from "react";
+import accounts from "../accounts";
+import {
+	Text,
+	useToast,
+	Tabs,
+	TabList,
+	Tab,
+	Stack,
+	HStack,
+	Spacer,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper,
+	Divider,
+	TabPanels,
+	TabPanel,
+	Button,
+	Center,
+} from "@chakra-ui/react";
+
+const formatter = new Intl.NumberFormat("en-US", {
+	style: "currency",
+	currency: "USD",
+});
+
+function TransactionPane(props: { symbol: string; price: number }) {
+	const [shares, setShares] = useState(1);
+	const [buyingPower, setBuyingPower] = useState(0);
+	const [availableShares, setAvailableShares] = useState(0);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const toast = useToast();
+
+	const submitTransaction = (
+		symbol: string,
+		quantity: number,
+		isBuy: boolean,
+	) => {
+		setIsLoading(true);
+		accounts
+			.makeTransaction(symbol, quantity, isBuy ? "buy" : "sell")
+			.then(() => {
+				// Show success toast on successful transaction
+				toast({
+					title: "Transaction submitted",
+					description: isBuy
+						? "Bought "
+						: "Sold " + quantity + " shares of " + symbol,
+					status: "success",
+				});
+				// Update buying power and available shares
+				accounts.getBuyingPower().then((value) => {
+					setBuyingPower(value);
+				});
+				accounts.getAvailableShares(symbol).then((value) => {
+					setAvailableShares(value);
+				});
+				// Turn off button spinner
+				setIsLoading(false);
+			})
+			.catch((err) => {
+				// Show error toast on failed transaction
+				toast({
+					title: "Error " + isBuy ? "buying" : "selling" + " " + symbol,
+					description: err.message,
+					status: "error",
+				});
+				// Turn off button spinner
+				setIsLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		accounts.getBuyingPower().then((value) => {
+			setBuyingPower(value);
+		});
+
+		accounts.getAvailableShares(props.symbol!).then((value) => {
+			setAvailableShares(value);
+		});
+	}, [location]);
+
+	return (
+		<>
+			<Tabs>
+				<TabList>
+					<Tab>Buy {props.symbol}</Tab>
+					<Tab>Sell {props.symbol}</Tab>
+				</TabList>
+
+				<Stack p="5">
+					<HStack>
+						<Text>Shares</Text>
+						<Spacer />
+						<NumberInput
+							defaultValue={1}
+							min={1}
+							width="20"
+							onChange={(e) => setShares(parseInt(e))}
+						>
+							<NumberInputField />
+							<NumberInputStepper>
+								<NumberIncrementStepper />
+								<NumberDecrementStepper />
+							</NumberInputStepper>
+						</NumberInput>
+					</HStack>
+					<HStack>
+						<Text>Current Price</Text>
+						<Spacer />
+						<Text>{formatter.format(props.price)}</Text>
+					</HStack>
+					<Spacer />
+					<Divider />
+					<Spacer />
+					<HStack fontWeight="bold">
+						<Text>Estimated Total</Text>
+						<Spacer />
+						<Text>{formatter.format(props.price * shares)}</Text>
+					</HStack>
+				</Stack>
+
+				<TabPanels>
+					<TabPanel>
+						<Button
+							colorScheme="teal"
+							size="lg"
+							width="100%"
+							onClick={() => submitTransaction(props.symbol!, shares, true)}
+							{...(isLoading ? { isLoading: true } : {})}
+						>
+							Buy
+						</Button>
+						<Center mt={3}>
+							<Text fontWeight="bold" fontSize="sm">
+								{formatter.format(buyingPower)} Buying Power Available
+							</Text>
+						</Center>
+					</TabPanel>
+					<TabPanel>
+						<Button
+							colorScheme="teal"
+							size="lg"
+							width="100%"
+							onClick={() => submitTransaction(props.symbol!, shares, false)}
+							{...(isLoading ? { isLoading: true } : {})}
+						>
+							Sell
+						</Button>
+						<Center mt={3}>
+							<Text fontWeight="bold" fontSize="sm">
+								{availableShares} Shares Available
+							</Text>
+						</Center>
+					</TabPanel>
+				</TabPanels>
+			</Tabs>
+		</>
+	);
+}
+
+export default TransactionPane;
--- app/src/pages/StockView.tsx
+++ app/src/pages/StockView.tsx
@@ -1,32 +1,9 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { useLocation, useParams } from "react-router-dom";
-import {
-	Text,
-	Tabs,
-	TabList,
-	TabPanels,
-	Tab,
-	TabPanel,
-	Stat,
-	StatArrow,
-	Heading,
-	Spacer,
-	Flex,
-	Box,
-	Button,
-	HStack,
-	Stack,
-	NumberInput,
-	NumberInputField,
-	NumberInputStepper,
-	NumberIncrementStepper,
-	NumberDecrementStepper,
-	Divider,
-	Center,
-	useToast,
-} from "@chakra-ui/react";
+import { Stat, StatArrow, Heading, Spacer, Flex, Box } from "@chakra-ui/react";
 import axios from "axios";
 import StockChart from "../components/StockChart";
+import TransactionPane from "../components/TransactionPane";
 import accounts from "../accounts";
 
 const formatter = new Intl.NumberFormat("en-US", {
@@ -38,26 +15,12 @@ function StockView() {
 	const { symbol } = useParams();
 	const location = useLocation();
 
-	const [shares, setShares] = useState(1);
-	const [buyingPower, setBuyingPower] = useState(0);
-	const [availableShares, setAvailableShares] = useState(0);
-
-	const toast = useToast();
-
 	const [stock, setStock] = useReducer(
 		(state: any, newState: any) => ({ ...state, ...newState }),
 		{ longName: "", symbol, price: 0, changePercent: 0 },
 	);
 
 	useEffect(() => {
-		accounts.getBuyingPower().then((value) => {
-			setBuyingPower(value);
-		});
-
-		accounts.getAvailableShares(symbol!).then((value) => {
-			setAvailableShares(value);
-		});
-
 		axios
 			.get(`http://localhost:3010/api/stocks/${symbol}/info`)
 			.then((res) => {
@@ -73,7 +36,7 @@ function StockView() {
 		<>
 			{stock.price > 0 && (
 				<Flex direction={{ base: "column-reverse", md: "row" }} gap={5}>
-					<Box flex="0.75">
+					<Box flex={accounts.isAuthenticated() ? "0.75" : "1"}>
 						<Stat>
 							<Heading size="md" fontWeight="md">
 								{stock.longName}
@@ -91,127 +54,11 @@ function StockView() {
 
 						<StockChart symbol={symbol as string} />
 					</Box>
-					<Box flex="0.25" borderWidth="1px" borderRadius="md" p={5}>
-						<Tabs>
-							<TabList>
-								<Tab>Buy {symbol}</Tab>
-								<Tab>Sell {symbol}</Tab>
-							</TabList>
-
-							<Stack p="5">
-								<HStack>
-									<Text>Shares</Text>
-									<Spacer />
-									<NumberInput
-										defaultValue={1}
-										min={1}
-										width="20"
-										onChange={(e) => setShares(parseInt(e))}
-									>
-										<NumberInputField />
-										<NumberInputStepper>
-											<NumberIncrementStepper />
-											<NumberDecrementStepper />
-										</NumberInputStepper>
-									</NumberInput>
-								</HStack>
-								<HStack>
-									<Text>Current Price</Text>
-									<Spacer />
-									<Text>{formatter.format(stock.price)}</Text>
-								</HStack>
-								<Spacer />
-								<Divider />
-								<Spacer />
-								<HStack fontWeight="bold">
-									<Text>Estimated Total</Text>
-									<Spacer />
-									<Text>{formatter.format(stock.price * shares)}</Text>
-								</HStack>
-							</Stack>
-
-							<TabPanels>
-								<TabPanel>
-									<Button
-										colorScheme="teal"
-										size="lg"
-										width="100%"
-										onClick={() =>
-											accounts
-												.makeTransaction(symbol!, shares, "buy")
-												.then(() => {
-													toast({
-														title: "Transaction submitted",
-														description:
-															"Bought " + shares + " shares of " + symbol,
-														status: "success",
-													});
-													accounts.getBuyingPower().then((value) => {
-														setBuyingPower(value);
-													});
-													accounts.getAvailableShares(symbol!).then((value) => {
-														setAvailableShares(value);
-													});
-												})
-												.catch((err) => {
-													toast({
-														title: "Error buying " + symbol,
-														description: err.message,
-														status: "error",
-													});
-												})
-										}
-									>
-										Buy
-									</Button>
-									<Center mt={3}>
-										<Text fontWeight="bold" fontSize="sm">
-											{formatter.format(buyingPower)} Buying Power Available
-										</Text>
-									</Center>
-								</TabPanel>
-								<TabPanel>
-									<Button
-										colorScheme="teal"
-										size="lg"
-										width="100%"
-										onClick={() =>
-											accounts
-												.makeTransaction(symbol!, shares, "sell")
-												.then(() => {
-													toast({
-														title: "Transaction submitted",
-														description:
-															"Sold " + shares + " shares of " + symbol,
-														status: "success",
-													});
-													accounts.getBuyingPower().then((value) => {
-														setBuyingPower(value);
-													});
-													accounts.getAvailableShares(symbol!).then((value) => {
-														setAvailableShares(value);
-													});
-												})
-												.catch((err) => {
-													toast({
-														title: "Error selling " + symbol,
-														description: err.message,
-														status: "error",
-													});
-												})
-										}
-									>
-										Sell
-									</Button>
-									<Center mt={3}>
-										<Text fontWeight="bold" fontSize="sm">
-											{availableShares} Shares Available
-										</Text>
-									</Center>
-								</TabPanel>
-							</TabPanels>
-						</Tabs>
-					</Box>
+					{accounts.isAuthenticated() && (
+						<Box flex="0.25" borderWidth="1px" borderRadius="md" p={5}>
+							<TransactionPane symbol={symbol as string} price={stock.price} />
+						</Box>
+					)}
 				</Flex>
 			)}
 		</>
